4c8a05d31499a31efa2522d316e2622f19cd4238
nick-iles <nick.assertthat@gmail.com>
2024-11-04 21:05:40 +0000

    Add Auth bearer token support

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'assertthat-bdd',         # How you named your package folder (MyLib)
   packages = ['assertthat_bdd'],   # Chose the same as "name"
-  version = '1.0.1',      # Start with a small number and increase it with every change you make
+  version = '1.2.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python tools for integration with AssertThat BDD plugin for Jira',   # Give a short description about your library
   author = 'Nick Iles',                   # Type in your name
   author_email = 'support@assertthat.com',      # Type in your E-Mail
   url = 'https://github.com/assertthat/assertthat-bdd-python',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/assertthat/assertthat-bdd-python/archive/v1.0.0.tar.gz',
+  download_url = 'https://github.com/assertthat/assertthat-bdd-python/archive/v1.2.0.tar.gz',
   keywords = ['assertthat', 'bdd', 'Jira', 'download features', 'upload features'],   # Keywords that define your package best
   install_requires=[
           'requests',
